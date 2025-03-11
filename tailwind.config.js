75db398ace083fc6f676ea0a54666c6ed51f419d
Sadfox-13 <raposaimortal8@gmail.com>
2025-03-10 21:23:47 -0300

    teste3

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        "home": "url('/Hamburgueria1/assets/bg.png')"
+      },
+    },
+  },
+  plugins: [],
+}
+
